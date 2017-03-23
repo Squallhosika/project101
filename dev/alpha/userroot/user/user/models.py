336a57c3444cd1367ac121249c5f8b0f40a12294
Kevin Glajean <kevin.glajean@gmail.com>
2017-03-23 00:15:40 +0000

    Fixing User migrations

--- dev/alpha/userroot/user/user/models.py
+++ dev/alpha/userroot/user/user/models.py
@@ -1,24 +1,14 @@
 from django.db import models
 
-
 class User(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, blank=True, default='')
 
-    class Meta:
-        ordering = ('created',)
-
-
-class Address(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
-    num = models.IntegerField()
-    street = models.CharField(max_length=100, blank=True, default='')
-    postcode = models.CharField(max_length=100, blank=True, default='')
-    city = models.CharField(max_length=100, blank=True, default='')
+    #menus = models.ManyToManyField(Menu, through='RNN_ClientMenu', blank=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
         ordering = ('created',)
+
--- dev/alpha/userroot/user/user/views.py
+++ dev/alpha/userroot/user/user/views.py
@@ -5,10 +5,3 @@ from user.user.serializers import *
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-class AddressViewSet(viewsets.ModelViewSet):
-    queryset = Address.objects.all()
-    serializer_class = AddressSerializer
-
-
