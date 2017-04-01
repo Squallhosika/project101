fd5c66267b1c14037f5779541500bf71459c9fb3
Pierre <pierre.poulaincormier@gmail.com>
2017-04-01 16:33:02 +0100

    Adding setup environment script for Linux

---
+++ dev/alpha/setup_env.sh
@@ -0,0 +1,10 @@
+echo "Start setup"
+rm -Rf env
+virtualenv env
+
+#*** Running the following command might be necessary
+#*** when mysql is not installed.
+#sudo apt-get install mysql-server libmysqlclient-dev
+env/bin/pip install -r requirement.txt
+
+echo "Setup done."
