825ecf1276fc2ea0278936c922b1ee49657ce139
Kevin Glajean <kevin.glajean@gmail.com>
2017-04-16 15:42:31 +0100

    Stack Deploy working with auto restart of web

--- dev/alpha/clientroot/docker-entrypoint.sh
+++ dev/alpha/clientroot/docker-entrypoint.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+#apt-get update
+#apt-get install netcat
+
+set -e
+
+#MYSQL_HOST=db
+#MYSQL_PORT=3306
+#WAIT=0
+
+#while ! nc -z $MYSQL_HOST $MYSQL_PORT; do
+#    sleep 3
+#    WAIT=$(($WAIT + 1))
+#      if [ "$WAIT" -gt 15 ]; then
+#        echo "Error: Timeout wating for Mysql to start"
+#        exit 1
+#      fi
+#done
+
 # Collect static files
-echo "Collect static files"
-python manage.py collectstatic --noinput
+#echo "Collect static files"
+#python manage.py collectstatic --noinput
 
 # Make migrations
 python manage.py makemigrations client
