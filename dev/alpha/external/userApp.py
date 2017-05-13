459516a859560b5d5fbcd15b2652e944263000b1
Kevin Glajean <kevin.glajean@gmail.com>
2017-05-13 16:34:31 +0100

    UserApp python simulation

--- dev/alpha/external/userApp.py
+++ dev/alpha/external/userApp.py
@@ -3,38 +3,15 @@ API_BASE_URL = 'http://127.0.0.1:8000/'
 API_CLIENT_URL = 'http://127.0.0.1:8000/'
 API_USER_URL = 'http://127.0.0.1:8000/'
 
-def get(name, **kwargs):
-
-    if name == 'items':     req = requests.get(API_BASE_URL + 'items/' + str(kwargs.get('id','')))
-    if name == 'menus':     req = requests.get(API_BASE_URL + 'menus/' + str(kwargs.get('id','')))
-    if name == 'clients':   req = requests.get(API_BASE_URL + 'clients/' + str(kwargs.get('id','')))
-
-    return req
-
-def getJSON(object, **kwargs):
-
-    if object == 'items':     req = requests.get(API_BASE_URL + 'items/' + str(kwargs.get('id','')))
-    if object == 'menus':     req = requests.get(API_BASE_URL + 'menus/' + str(kwargs.get('id','')))
-    if object == 'clients':   req = requests.get(API_BASE_URL + 'clients/' + str(kwargs.get('id','')))
-
-    return req.json()
-
-def create(object, **kwargs):
-    json = '{"name": "' + str(kwargs.get('name','')) + '"}'
-    data = {'name': str(kwargs.get('name',''))}
-    req = requests.post(API_BASE_URL + 'items/',data=data)
-
-    if object == 'items':     req = requests.get(API_BASE_URL + 'items/' + str(kwargs.get('id','')))
-
-
-
 def call_function(method_name, service_name, function_name, params=None):
     url = get_url(service_name, function_name)
 
     if method_name == 'GET':        req = requests.get(url, params=params)
     elif method_name == 'POST':     req = requests.post(url, data=params)
+    elif method_name == 'DELETE':   req = requests.delete(url, data=params)
 
-    return req.json()
+    return req
+
 
 def get_url(service_name, function_name):
     if service_name == 'client':    url = API_CLIENT_URL
@@ -48,14 +25,19 @@ def get_url(service_name, function_name):
 if __name__ == '__main__':
 
     service_name = 'client'
-    function_name = 'items'
-    req0_json = call_function('GET', service_name, function_name)
-    print(req0_json)
+    function_name = 'clientaround'
+    req0 = call_function('GET', service_name, function_name)
+    print(req0.status_code)
+    print(req0.json())
 
-    params = {'name': 'item4'}
-    req1_json = call_function('POST', service_name, function_name, params)
-    req2_json = call_function('GET', service_name, function_name)
-    print(req2_json)
+    # params = {'name': 'item4'}
+    # req1 = call_function('POST', service_name, function_name, params)
+
+    # params = {'id': '5'}
+    # req1 = call_function('DELETE', service_name, function_name, params)
+    # req2 = call_function('GET', service_name, function_name)
+    # print(req1.status_code)
+    # print(req2.json())
 
     # req1 = get('items', id=1)
     # req1_json = getJSON('items')
