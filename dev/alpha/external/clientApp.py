c00fc774028b171a6d07adea05497f2f8d28e7eb
Jonathan <jonathan.lerch@gmail.com>
2017-05-29 20:27:08 +0100

    Client User Gui a first operational verion

--- dev/alpha/external/clientApp.py
+++ dev/alpha/external/clientApp.py
@@ -23,19 +23,33 @@ def get_url(service_name, function_name):
     return url + service_name + '/' + function_name + '/'
 
 
-class ClientApp():
+class ClientApp:
 
-    def __init__(self, client_id, shift_id):
+    def __init__(self, client_id):
         self.client_id = client_id
-        self.shift_id = shift_id
-        self.pending_order = None
-        self.beingserve_order = None
-        self.inqueue_order = None
+        # TODO implement a logic for the shift
+        self.shift_id = client_id
+        self.pending_order = []
+        self.beingserve_order = []
+        self.inqueue_order = []
         self.active_employees = []
         self.update_pending_order()
-        # self.update_beingserve_order()
-        # self.update_inqueue_order()
+        self.update_beingserve_order()
+        self.update_inqueue_order()
         # self.update_active_employees()
+
+    @staticmethod
+    def orders_to_ids(key, orders):
+        return [order[key] for order in orders]
+
+    def pending_order_ids(self):
+        return self.orders_to_ids('order_id', self.pending_order)
+
+    def beingserve_order_ids(self):
+        return self.orders_to_ids('order_id', self.beingserve_order)
+
+    def inqueue_order_ids(self):
+        return self.orders_to_ids('order_id', self.inqueue_order)
 
     def update_pending_order(self):
         req_new_orders = call_function('GET', 'order', 'neworders', {'client_id': self.client_id})
@@ -47,41 +61,47 @@ class ClientApp():
     def validate_next_pending_order(self):
         order = self.next_pending_order()
         call_function('PUT', 'order', 'orderacceptedbyclient', {'order_id': order['order_id']})
+        self.update_pending_order()
 
     def reject_next_pending_order(self):
         order = self.next_pending_order()
         call_function('PUT', 'order', 'orderrejectedbyclient', {'order_id': order['order_id']})
+        self.update_pending_order()
 
     def update_beingserve_order(self):
-        req_new_orders = call_function('GET', 'order', 'beingserveorder', {'client_id': self.client_id})
+        req_new_orders = call_function('GET', 'order', 'beingserveorders', {'client_id': self.client_id})
         self.beingserve_order = req_new_orders.json()
 
     def complete_order(self, order_id):
-        call_function('PUT', 'order', 'order_completed', {'order_id': order_id})
-        self.beingserve_order = filter(lambda x: x['order_id'] == order_id, self.beingserve_order)
+        call_function('PUT', 'order', 'ordercompleted', {'order_id': order_id})
+        list_completed = list(filter(lambda x: x['order_id'] == order_id, self.beingserve_order))
+        self.beingserve_order.remove(list_completed[0])
 
     def complete_order_by_employee_id(self, employee_id):
-        order_id = filter(lambda x: x['employee_id'] == employee_id, self.beingserve_order)[0]['order_id']
+        list_order = list(filter(lambda x: x['employee_id'] == employee_id, self.beingserve_order))
+        order_id = list_order[0]['order_id']
         self.complete_order(order_id)
 
     def attribute_next_order(self, employee_id):
-        order = call_function('GET', 'order', 'nexttoserve', {'client_id': self.client_id, 'employee_id': employee_id})
+        order = call_function('PUT', 'order', 'nexttoserve', {'client_id': self.client_id, 'employee_id': employee_id})
         self.beingserve_order.append(order.json())
+        self.update_inqueue_order()
         return order
 
     def update_inqueue_order(self):
-        req_inqueue_orders = call_function('GET', 'order', 'inqueueorder', {'client_id': self.client_id})
+        req_inqueue_orders = call_function('GET', 'order', 'inqueueorders', {'client_id': self.client_id})
         self.inqueue_order = req_inqueue_orders.json()
 
     def add_employee_to_shift(self, employee_id):
         call_function('POST', 'client', 'addemployeetoshift',
-                      {"employee_id": employee_id,  "shift_id": self.shift_id, "status": ""})
+                      {"employee_id": employee_id, "shift_id": self.shift_id, "status": ""})
         self.active_employees.append(employee_id)
 
     def remove_employee_from_shift(self, employee_id):
-        call_function('DELETE', 'client', 'removeemployeefromshift', {'employee_id': employee_id})
+        call_function('DELETE', 'client', 'removeemployeefromshift',
+                      {"shift_id": self.shift_id, 'employee_id': employee_id})
         self.active_employees.remove(employee_id)
 
     def update_active_employees(self):
         employees = call_function('GET', 'client', 'getemployeesfromshift', {'shift_id': self.shift_id})
-        self.active_employees = [employee['id'] for employee in employees]
+        self.active_employees = [employee['id'] for employee in employees.json()]
--- dev/alpha/orderroot/order/order/views.py
+++ dev/alpha/orderroot/order/order/views.py
@@ -223,7 +223,7 @@ def last_orderflow(request):
         return order_id_to_last_flow(request)
 
 
-@api_view(['GET'])
+@api_view(['PUT'])
 def next_to_serve(request):
     client_id = request.data['client_id']
     employee_id = request.data['employee_id']
@@ -232,13 +232,18 @@ def next_to_serve(request):
     except Queue.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        pos = queue.position + 1
+    if request.method == 'PUT':
+        pos = queue.position
         while pos <= queue.last_rank:
+            pos += 1
             try:
-                order_flow = Queue.objects.get(client_id=client_id, rank=pos)
+                order_flow = OrderFlow.objects.get(client_id=client_id, rank=pos)
+                if order_flow.status != 'accepted_by_client':
+                    continue
             except OrderFlow.DoesNotExist:
                 continue
-            new_flow = create_next_flow(order_flow, 'beingServe', employee_id)
-            serializer = OrderFlowSerializer(new_flow.order, context={'request': request})
+            order_flow.employee_id = employee_id
+            order_flow.status = 'beingServe'
+            order_flow.save()
+            serializer = OrderFlowSerializer(order_flow, context={'request': request})
             return Response(serializer.data)
