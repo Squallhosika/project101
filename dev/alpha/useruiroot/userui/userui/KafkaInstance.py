892dc824ff15d6f43304535bdf66b946599662b4
Jonathan <jonathan.lerch@gmail.com>
2017-08-14 22:16:29 +0100

    Kafka an operational working and tested version 2

--- dev/alpha/useruiroot/userui/userui/KafkaInstance.py
+++ dev/alpha/useruiroot/userui/userui/KafkaInstance.py
@@ -1,6 +1,5 @@
 from core.kafka.KEventManager import KEventManager
 from core.kafka.KSettings import KSettings
-from core.kafka.KEvent import KEvent
 
 event_to_fct = \
     {'order':
@@ -22,6 +21,4 @@ kSettings = KSettings(event_to_fct, host, port, topics, groups)
 kEventManager = KEventManager(kSettings)
 
 if __name__ == '__main__':
-    event = KEvent(sender, topics[0], event_type, 'create_order', 'nothing')
-    kEventManager.kgenerator.publish(event)
     kEventManager.start()
